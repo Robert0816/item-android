e7c8c58c71fac401c2fef32061339da12c8f85ce
Robert Duschek <robert.duschek@telekom.de>
2020-05-27 17:22:39 +0200

    add model Item and add ItemApi

--- app/src/main/java/de/telekom/sea/mystuff/frontend/android/MyStuffApplication.java
+++ app/src/main/java/de/telekom/sea/mystuff/frontend/android/MyStuffApplication.java
@@ -2,7 +2,32 @@ package de.telekom.sea.mystuff.frontend.android;
 
 import android.app.Application;
 
+import lombok.Getter;
+
 public class MyStuffApplication extends Application {
+
+    @Getter
+    private static MyStuffApplication instance;
+
+    @Getter
+    private MyStuffContext myStuffContext;
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+        instance = this;
+        this.myStuffContext = new MyStuffContext().init(this);
+    }
+
+    @Override
+    public void onTerminate() {
+
+
+        super.onTerminate();
+        instance = null;
+    }
+
 
 }
 
