757e14a32a1ffff145b6523f3d5a3f6466e9844f
Robert Duschek <robert.duschek@telekom.de>
2020-05-31 23:25:50 +0200

    old MainAct

--- app/src/main/java/de/telekom/sea/mystuff/frontend/android/MainActivity.java
+++ app/src/main/java/de/telekom/sea/mystuff/frontend/android/MainActivity.java
@@ -1,33 +1,93 @@
 package de.telekom.sea.mystuff.frontend.android;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import android.os.Bundle;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import java.util.ArrayList;
 
 import de.telekom.sea.mystuff.frontend.android.ui.ItemListRecyclerViewAdapter;
 import de.telekom.sea.mystuff.frontend.android.ui.ItemListViewModel;
 
+
 public class MainActivity extends AppCompatActivity {
-    ItemListRecyclerViewAdapter adapter;
-    ItemListViewModel viewModel;
+
+//    private DirectChatListViewModel viewModel;
+//    private DirectChatListAdapter directChatListAdapter;
+
+    private ItemListViewModel itemListViewModel;
+    private ItemListRecyclerViewAdapter itemListRecyclerViewAdapter;
+    private RecyclerView itemList;
+
+
+//   MyListRideFragment (Mayo)
+/*    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = new ViewModelProvider(this).get(MyListViewModel.class);
+        viewModel.initWithApplication(getActivity().getApplication());
+        ridesListAdapter = new RidesListAdapter(new ArrayList<>(), this);
+
+        ridesSearchList.setLayoutManager(new LinearLayoutManager(getContext()));
+        ridesSearchList.setAdapter(ridesListAdapter);
+
+        loadAndObserveSearchRideViewModel();
+
+
+    }
+    RecyclerView recyclerView = findViewById(R.id.recycler_view);
+    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+    recyclerView.setLayoutManager(layoutManager);
+    */
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewModel = new ViewModelProvider(this).get(ItemListViewModel.class);
-        viewModel.initWithApplication(getApplication());
-        adapter = new ItemListRecyclerViewAdapter(new ArrayList<>());
-        RecyclerView rv = findViewById(R.id.rv_items);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        rv.setAdapter(adapter);
-        viewModel.loadItems().observe(this, listApiResponse -> {
-            if (listApiResponse.isSuccessful()) {
-                adapter.updateList(listApiResponse.body);
+        itemList = findViewById(R.id.rv_items);
+
+
+        itemListViewModel = new ViewModelProvider(this).get(ItemListViewModel.class);
+        itemListViewModel.initWithApplication(getApplication());
+        itemListRecyclerViewAdapter = new ItemListRecyclerViewAdapter(new ArrayList<>());
+        itemList.setLayoutManager(new LinearLayoutManager(this));
+        itemList.setAdapter(itemListRecyclerViewAdapter);
+        loadAndObserveViewModel();
+    }
+
+
+  /*  private void loadAndObserveSearchRideViewModel() {
+        viewModel.getMyRides().observe(getViewLifecycleOwner(), apiResponse -> {
+            if (apiResponse.isSuccessful()) {
+                ridesSearchList.setVisibility(View.VISIBLE);
+                ridesListAdapter.updateList(apiResponse.body);
+                searchListError.setVisibility(View.GONE);
             } else {
-                MyStuffContext ctx = ((MyStuffApplication)getApplication()).getMyStuffContext();
-                ctx.sendInfoMessage("Could not load the list");
+                ((MayoApplication) getActivity().getApplication()).getMayoContext().sendInfoMessage(apiResponse.errorMessage);
+                ridesSearchList.setVisibility(View.GONE);
+                searchListError.setVisibility(View.VISIBLE);
+            }
+            loadingView.setVisibility(View.GONE);
+
+        });*/
+
+
+    private void loadAndObserveViewModel() {
+        itemListViewModel.loadItems().observe(this, listApiResponse -> {
+            if (listApiResponse.isSuccessful()) {
+                itemListRecyclerViewAdapter.updateList(listApiResponse.body);
+            } else {
+                ((MyStuffApplication) getApplication()).getMyStuffContext().sendInfoMessage(listApiResponse.errorMessage);
             }
         });
     }
